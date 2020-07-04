df145eda674765145abfcfbbcc7ae5a064643f0c
SergSerg <sergio.ne@inbox.ru>
2020-07-04 21:51:36 +0600

    Создал observable метод getMessage() в Presenter.

--- TestRxLambButKnf/app/src/main/java/com/example/testrxlambbutknf/Presenter.java
+++ TestRxLambButKnf/app/src/main/java/com/example/testrxlambbutknf/Presenter.java
@@ -1,8 +1,32 @@
 package com.example.testrxlambbutknf;
 
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.schedulers.Schedulers;
+
 public class Presenter {
     private static final String TAG = "Presenter";
 
-    public
+    public Observable<String> getMessage() {
+        Observable<String> observable = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            try {
+                for (int i = 0; i < 15; i++) {
+                    TimeUnit.SECONDS.sleep(1);
+                    String msg = "message: " + i;
+                    Log.d(TAG, "getMessage: " + Thread.currentThread().getName() + ": " + msg);
+                    emitter.onNext(msg);
+                }
+                emitter.onComplete();
+            } catch (InterruptedException e) {
+                Log.d(TAG, "getMessage: not disposed");
+            }
+        }).subscribeOn(Schedulers.io());
+
+        return observable;
+    }
 
 }
