7b666f15a69dc980290c9f54f14ee508cbff967f
SergSerg <sergio.ne@inbox.ru>
2020-07-04 23:21:11 +0600

    Создал класс Presenter.

--- TestSingle/app/src/main/java/com/example/testsingle/presenter/Presenter.java
+++ TestSingle/app/src/main/java/com/example/testsingle/presenter/Presenter.java
@@ -1,10 +1,14 @@
 package com.example.testsingle.presenter;
 
 
+import android.util.Log;
+
 import com.example.testsingle.model.Model;
 import com.example.testsingle.view.MainView;
 
 import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import moxy.InjectViewState;
 import moxy.MvpPresenter;
 
@@ -18,9 +22,10 @@ public class Presenter extends MvpPresenter<MainView> {
     }
     public void request(){
         Single<String> single = model.requestToServer();
-
+        Disposable disposable = single.observeOn(AndroidSchedulers.mainThread()).subscribe(json ->{
+            Log.d(TAG, "request: " + Thread.currentThread().getName() + ": " + json);
+            getViewState().setText(json);
+        });
     }
-
-
-
 }
+
