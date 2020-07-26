e1359f9778bbe3b310158e828a007c64321570bb
SergSerg <sergio.ne@inbox.ru>
2020-07-26 08:32:17 +0600

    Добавил изменения в макет.

--- TestRxLambButKnf/app/src/main/java/com/example/testrxlambbutknf/RetrofitActivity.java
+++ TestRxLambButKnf/app/src/main/java/com/example/testrxlambbutknf/RetrofitActivity.java
@@ -3,16 +3,12 @@ package com.example.testrxlambbutknf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class RetrofitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_retrofit);
     }
 }
